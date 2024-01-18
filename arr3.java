fdf9fff6e4633be8e2b6f6c1ce535efac5b18c13
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-18 16:57:22 +0400

    Update arr3.java

--- arr3.java
+++ arr3.java
@@ -1,10 +1,10 @@
-public class arr3{
-	public static void main(String[] arg){
-		int[][] a=new int[3][];
-		for (int i=0; i<a.length; i++)
-			a[i]=new int[3];
-		a[0][0]=12;
-		System.out.print(a[0][0]);
-		
-	}
+public class arr3 {
+  public static void main(String[] arg) {
+    int[][] a = new int[3][];
+    for (int i = 0; i < a.length; i++)
+      a[i] = new int[3];
+    a[0][0] = 12;
+    System.out.print(a[0][0]);
+
+  }
 }
